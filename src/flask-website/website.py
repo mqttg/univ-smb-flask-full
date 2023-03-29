becb4a393e7dc51e33c41d1b5668524f801944bc
Matteo Gamba <matt.gamba.chabert@gmail.com>
2023-03-29 17:07:16 +0000

    19h

--- src/flask-website/website.py
+++ src/flask-website/website.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
@@ -6,3 +6,100 @@ app = Flask(__name__)
 def start(): 
     return render_template('start.html')
     
+# Route pour la page de connexion
+@app.route('/login')
+def login():
+    return 'Page de connexion'
+
+# Route pour la déconnexion
+@app.route('/logout')
+def logout():
+    return 'Page de déconnexion'
+
+# Route pour la liste des profils utilisateur
+@app.route('/list_profile')
+def list_profile():
+    return 'Liste des profils utilisateur'
+
+# Route pour afficher le profil d'un utilisateur en particulier
+@app.route('/profile/<username>')
+def profile(username):
+    return 'Profil de l\'utilisateur ' + username
+
+# Route pour la liste complète de toutes les configurations
+@app.route('/list_all')
+def list_all():
+    return 'Liste complète de toutes les configurations'
+
+# Route pour la liste des Load Balancer
+@app.route('/lib/list')
+def lib_list():
+    return 'Liste des Load Balancer'
+
+# Route pour afficher la configuration d'un Load Balancer en particulier
+@app.route('/lib/<id>')
+def lib_config(id):
+    return 'Configuration du Load Balancer ' + id
+
+# Route pour créer un nouveau Load Balancer
+@app.route('/lib/create', methods=['GET', 'POST'])
+def lib_create():
+    if request.method == 'POST':
+        # Traitement des données de formulaire
+        return redirect(url_for('lib_list'))
+    else:
+        return render_template('templates/lib_create.html') #Page création lb
+
+# Route pour supprimer un Load Balancer
+@app.route('/lb/<id>', methods=['DELETE'])
+def lb_delete(id):
+    # Supprimer le Load Balancer avec l'ID spécifié
+    return 'Load Balancer ' + id + ' supprimé'
+
+# Route pour la liste des Reverse Proxy
+@app.route('/rp/list')
+def rp_list():
+    return 'Liste des Reverse Proxy'
+
+# Route pour afficher la configuration d'un Reverse Proxy en particulier
+@app.route('/rp/<id>')
+def rp_config(id):
+    return 'Configuration du Reverse Proxy ' + id
+
+# Route pour créer un nouveau Reverse Proxy
+@app.route('/rp/create', methods=['GET', 'POST'])
+def rp_create():
+    if request.method == 'POST':
+        # Traitement des données de formulaire
+        return redirect(url_for('rp_list'))
+    else:
+        return render_template('rp_create.html') #Page création rp
+
+# Route pour supprimer un Reverse Proxy
+@app.route('/rp/<id>', methods=['DELETE'])
+def rp_delete(id):
+    # Supprimer le Reverse Proxy avec l'ID spécifié
+    return 'Reverse Proxy ' + id + ' supprimé'
+
+# Route pour la liste des serveurs Web
+@app.route('/ws/list')
+def ws_list():
+    return 'Liste des serveurs Web'
+
+# Route pour afficher la configuration d'un serveur Web en particulier
+@app.route('/ws/<id>')
+def ws_config(id):
+    return 'Configuration du serveur Web ' + id
+
+# Route pour créer un nouveau serveur Web
+@app.route('/ws/create', methods=['GET', 'POST'])
+def ws_create():
+    if request.method == 'POST':
+        # Traitement des données de formulaire
+        return redirect(url_for('ws_list'))
+    else:
+        return render_template('ws_create.html') #Créer un serveur web
+
+# Route pour supprimer un serveur Web
+@app.route('/ws/<id>', methods=['DELETE'])
+def ws
