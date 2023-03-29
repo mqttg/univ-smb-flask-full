44dfad183f7432c555fdb5b867b5d8f8a513c610
Matteo Gamba <matt.gamba.chabert@gmail.com>
2023-03-29 17:22:54 +0000

    19h22

--- src/flask-api/api.py
+++ src/flask-api/api.py
@@ -1,7 +1,88 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, API!"
+
+# Route pour la page de connexion
+@app.route('/login')
+def login():
+    return 'Page de connexion'
+
+# Route pour la liste des utilisateurs
+@app.route('/identity')
+def identity():
+    return 'Liste des utilisateurs'
+
+# Route pour afficher les informations d'un utilisateur en particulier
+@app.route('/identity/<username>')
+def user_info(username):
+    return 'Informations de l\'utilisateur ' + username
+
+# Route pour la liste des Load Balancer
+@app.route('/config/lb')
+def lb_list():
+    return 'Liste des Load Balancer'
+
+# Route pour afficher la configuration d'un Load Balancer en particulier
+@app.route('/config/lb/<id>')
+def lb_config(id):
+    return 'Configuration du Load Balancer ' + id
+
+# Route pour créer un nouveau Load Balancer
+@app.route('/config/lb', methods=['POST'])
+def lb_create():
+    # Traitement des données de formulaire pour créer un nouveau Load Balancer
+    return redirect(url_for('lb_list'))
+
+# Route pour supprimer un Load Balancer
+@app.route('/config/lb/<id>', methods=['DELETE'])
+def lb_delete(id):
+    # Supprimer le Load Balancer avec l'ID spécifié
+    return 'Load Balancer ' + id + ' supprimé'
+
+# Route pour la liste des Reverse Proxy
+@app.route('/config/rp')
+def rp_list():
+    return 'Liste des Reverse Proxy'
+
+# Route pour afficher la configuration d'un Reverse Proxy en particulier
+@app.route('/config/rp/<id>')
+def rp_config(id):
+    return 'Configuration du Reverse Proxy ' + id
+
+# Route pour créer un nouveau Reverse Proxy
+@app.route('/config/rp', methods=['POST'])
+def rp_create():
+    # Traitement des données de formulaire pour créer un nouveau Reverse Proxy
+    return redirect(url_for('rp_list'))
+
+# Route pour supprimer un Reverse Proxy
+@app.route('/config/rp/<id>', methods=['DELETE'])
+def rp_delete(id):
+    # Supprimer le Reverse Proxy avec l'ID spécifié
+    return 'Reverse Proxy ' + id + ' supprimé'
+
+# Route pour la liste des serveurs Web
+@app.route('/config/ws')
+def ws_list():
+    return 'Liste des serveurs Web'
+
+# Route pour afficher la configuration d'un serveur Web en particulier
+@app.route('/config/ws/<id>')
+def ws_config(id):
+    return 'Configuration du serveur Web ' + id
+
+# Route pour créer un nouveau serveur Web
+@app.route('/config/ws', methods=['POST'])
+def ws_create():
+    # Traitement des données de formulaire pour créer un nouveau serveur Web
+    return redirect(url_for('ws_list'))
+
+# Route pour supprimer un serveur Web
+@app.route('/config/ws/<id>', methods=['DELETE'])
+def ws_delete(id):
+    # Supprimer le serveur Web avec l'ID spécifié
+    return 'Serveur Web ' + id + ' supprimé'
